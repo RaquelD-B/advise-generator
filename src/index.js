8e9e745d2a3f6f88ec13fcdeb5b961c528069ad9
RaquelD-B <154616671+RaquelD-B@users.noreply.github.com>
2024-05-14 08:33:03 -0400

    Added basic javascript and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function generateAdvice(event) {
+  event.preventDefault();
 
+  new Typewriter(".advice-container", {
+    strings: ["Drink water"],
+    autoStart: true,
+    cursor: "",
+  });
+}
+let adviceFormElement = document.querySelector("#advice-generator-form");
+adviceFormElement.addEventListener("submit", generateAdvice);
